1176196cdbc6003cec96716ed05d93eddd8eac7b
Roland Netzsch <roland.netzsch@encode.moe>
2019-10-31 12:44:05 +0100

    Lolz

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -1,4 +1,21 @@
-#!/usr/bin/bash
-sudo update-alternatives --set vim $(which nvim) 
+#!/bin/bash
+sudo apt update
+sudo apt upgrade
+
+sudo apt install neovim zsh
+
 sudo update-alternatives --set editor $(which nvim)
-sudo chsh -s $(which zsh) $(whoami)
+sudo update-alternatives --set vim $(which nvim)
+sudo update-alternatives --set vi $(which nvim)
+
+git config --global user.email "stuxcrystal@encode.moe"
+git config --global user.name "StuxCrystal"
+
+git --git-dir=/home/netzschr/.dotfiles --work-tree=/home/netzschr init /home/netzschr
+git --git-dir=/home/netzschr/.dotfiles --work-tree=/home/netzschr remote add origin git@github.com:stuxcrystal/dotfiles
+git --git-dir=/home/netzschr/.dotfiles --work-tree=/home/netzschr config --local status.showUntrackedFiles no
+git --git-dir=/home/netzschr/.dotfiles --work-tree=/home/netzschr pull origin master
+git --git-dir=/home/netzschr/.dotfiles --work-tree=/home/netzschr git submodules update --init --recursive
+
+chsh -s $(which zsh) $(id -un)
+zsh
