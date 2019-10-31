e2c8af73976b5916ba009b51188e56a829c89fea
Roland Netzsch <roland.netzsch@encode.moe>
2019-10-31 12:45:30 +0100

    Remote actual pathes

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -11,11 +11,11 @@ sudo update-alternatives --set vi $(which nvim)
 git config --global user.email "stuxcrystal@encode.moe"
 git config --global user.name "StuxCrystal"
 
-git --git-dir=/home/netzschr/.dotfiles --work-tree=/home/netzschr init /home/netzschr
-git --git-dir=/home/netzschr/.dotfiles --work-tree=/home/netzschr remote add origin git@github.com:stuxcrystal/dotfiles
-git --git-dir=/home/netzschr/.dotfiles --work-tree=/home/netzschr config --local status.showUntrackedFiles no
-git --git-dir=/home/netzschr/.dotfiles --work-tree=/home/netzschr pull origin master
-git --git-dir=/home/netzschr/.dotfiles --work-tree=/home/netzschr git submodules update --init --recursive
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME init $HOME
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME remote add origin git@github.com:stuxcrystal/dotfiles
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME pull origin master
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME git submodules update --init --recursive
 
 chsh -s $(which zsh) $(id -un)
 zsh
